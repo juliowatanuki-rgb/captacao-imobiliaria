0cafc8ffe86fdcb0112d4a850464b0300b4602dc
Julio Watanuki <juliowatanuki@gmail.com>
2026-07-28 17:48:51 -0300

    Corrige M2M para filtrar so Praia Grande (mesmo bug do Igor Braga)
    Revalidacao completa (todas as 34 paginas, nao so uma amostra) encontrou
    que a urlListagem antiga ("/a-venda", sem filtro de cidade) trazia
    inventario de varias cidades do litoral: so 589 de 660 imoveis eram de
    Praia Grande (o resto era Mongagua, Itanhaem, Sao Vicente, Santos,
    Peruibe). Trocada para a URL dedicada por cidade "/a-venda/praia-grande-sp"
    (mesmo motor Coruja Sistemas, mesmo padrao ja usado em
    igor_braga_imoveis/tempone_imoveis_pg/dema_imoveis).
    
    Revalidado com a URL corrigida: 380 imoveis, 20 paginas, 100% de
    cidade="PRAIA GRANDE - SP", 0 externalId duplicado, 0 url relativa.
    
    Ainda NAO executado contra o Neon.

--- crawlers/src/sites/m2m_imoveis.ts
+++ crawlers/src/sites/m2m_imoveis.ts
@@ -1,9 +1,18 @@
 // Inspecionado ao vivo em 2026-07-28 contra https://m2mimoveis.com.br.
 // Plataforma real: Coruja Sistemas (confirma o chute do seed).
+//
+// A urlListagem antiga ("/a-venda", sem filtro de cidade) retorna inventario
+// de VARIAS cidades do litoral (so 589 de 660 imoveis eram de Praia Grande
+// numa revalidacao completa em 2026-07-28 - o resto era Mongagua, Itanhaem,
+// Sao Vicente, Santos, Peruibe etc). Trocada para a URL dedicada por cidade
+// (mesmo padrao de igor_braga_imoveis/tempone_imoveis_pg/dema_imoveis):
+// https://m2mimoveis.com.br/a-venda/praia-grande-sp
+// Revalidado ao vivo com essa URL: 380 imoveis, 20 paginas, 100% com
+// cidade="PRAIA GRANDE - SP", 0 externalId duplicado, 0 urlOriginal relativa.
 // Crawler ainda NAO executado contra o Neon de producao - preparado para
 // validacao/uso futuro.
 import { createCorujaCrawler } from "../platforms/coruja.js";
 
 export default createCorujaCrawler({
-  urlListagem: "https://m2mimoveis.com.br/a-venda",
+  urlListagem: "https://m2mimoveis.com.br/a-venda/praia-grande-sp",
 });
