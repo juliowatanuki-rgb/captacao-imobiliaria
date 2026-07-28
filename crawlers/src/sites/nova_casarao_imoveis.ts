4bac073d0ff493e2cdb13173137ed67300cda746
Julio Watanuki <juliowatanuki@gmail.com>
2026-07-28 18:26:36 -0300

    Corrige Nova Casarao para filtrar so Praia Grande (mesmo bug do Igor Braga/M2M)
    Revalidacao completa (280 paginas) encontrou 31 de 3375 imoveis de outras
    cidades (Mongagua, Itanhaem, Sao Vicente, Guaruja, Joanopolis) na
    urlListagem antiga ("/imoveis/a-venda", sem filtro). Encontrada a URL
    dedicada por cidade (via link de bairro no rodape, removendo o segmento de
    bairro): https://www.novacasarao.com.br/imoveis/a-venda/praia-grande
    
    Revalidado com a URL corrigida: 3344 imoveis, 280 paginas, 100% de
    cidade="Praia Grande", 0 externalId duplicado.
    
    Ainda NAO executado contra o Neon.

--- crawlers/src/sites/nova_casarao_imoveis.ts
+++ crawlers/src/sites/nova_casarao_imoveis.ts
@@ -2,10 +2,20 @@
 // (o dominio sem "www." retorna erro 552 - sempre usar www.).
 // Plataforma real: Kenlo (o seed original tinha "Union Softwares" como chute,
 // nao confirmado).
+//
+// A urlListagem antiga ("/imoveis/a-venda", sem filtro de cidade) retorna
+// inventario de varias cidades (31 de 3375 imoveis numa revalidacao completa
+// em 2026-07-28 eram de Mongagua/Itanhaem/Sao Vicente/Guaruja/Joanopolis, nao
+// Praia Grande). Trocada para a URL dedicada por cidade (encontrada via um
+// link de bairro no rodape, no formato /imoveis/a-venda/praia-grande/{bairro}
+// - removendo o segmento de bairro filtra pela cidade toda):
+// https://www.novacasarao.com.br/imoveis/a-venda/praia-grande
+// Revalidado com essa URL: 3344 imoveis, 280 paginas, 100% cidade="Praia
+// Grande", 0 externalId duplicado.
 // Crawler ainda NAO executado contra o Neon de producao - preparado para
 // validacao/uso futuro.
 import { createKenloCrawler } from "../platforms/kenlo.js";
 
 export default createKenloCrawler({
-  urlListagem: "https://www.novacasarao.com.br/imoveis/a-venda",
+  urlListagem: "https://www.novacasarao.com.br/imoveis/a-venda/praia-grande",
 });
